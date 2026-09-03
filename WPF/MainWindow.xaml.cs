f706fb318ebaa976deabeee3437026847089b8de
sanyi68-dszc <szatmari.sandor.68@dszcbaross.edu.hu>
2026-09-03 11:30:53 +0200

    Grafikusan kész, események nincsenek

---
+++ WPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Varos> varosok = new List<Varos>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Adatbeolvas();
+        }
+
+        private void Adatbeolvas()
+        {
+            using (StreamReader olvas = new StreamReader("Varosok.csv"))
+            {
+
+            }
+        }
+
+        private void LB_Varosok_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void BT_Keres_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void BT_UjKeres_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ WPF/Varos.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF
+{
+    class Varos
+    {
+        public Varos(string nev, string jaras, string kisterseg, int nepesseg, double terulet)
+        {
+            Nev = nev;
+            Jaras = jaras;
+            Kisterseg = kisterseg;
+            Nepesseg = nepesseg;
+            Terulet = terulet;
+        }
+
+        public string Nev { get; set; }
+        public string Jaras { get; set; }
+        public string Kisterseg { get; set; }
+        public int Nepesseg { get; set; }
+        public double Terulet { get; set; }
+    }
+}
